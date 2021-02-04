58568616c78a9a7d7bbc5218c76a661c483fe4f8
julien willette <julien.willette@gmail.com>
2021-02-04 13:20:47 +0100

    add filter

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,20 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Wine;
+use App\Data\SearchData;
+use App\Form\SearchForm;
 use App\Repository\WineRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
     /**
      * @Route("/produit", name="product")
      */
-    public function index(WineRepository $wineReposytory): Response
+    public function index(WineRepository $wineReposytory, Request $request): Response
     {
+        $data = new SearchData();
+        $form = $this->createForm(SearchForm::class, $data);
+        $form->handleRequest($request);
+        $wineReposytory = $wineReposytory->findSearch($data);
         return $this->render('product/index.html.twig', [
-            'wine' => $wineReposytory->findAll(),
+            'wine' => $wineReposytory,
+            'form' => $form->createView()
         ]);
     }
 
--- src/Repository/WineRepository.php
+++ src/Repository/WineRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Wine;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use App\Data\SearchData;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @method Wine|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +18,59 @@ class WineRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Wine::class);
+    }
+
+    /**
+     * Récupère les produits en lien avec une recherche
+     * @return Wine[]
+     */
+    public function findSearch(SearchData $search): array
+    {
+        $query = $this
+            ->createQueryBuilder('p')
+            ->select('c', 'n', 'r', 'p')
+            ->join('p.color', 'c')
+            ->join('p.naming', 'n')
+            ->join('p.region', 'r');
+
+            if(!empty($search->q)) {
+                $query = $query
+                    ->andWhere('p.name LIKE :q')
+                    ->setParameter('q', "%{$search->q}%");
+            }
+
+            if(!empty($search->min)) {
+                $query = $query
+                    ->andWhere('p.price >= :min')
+                    ->setParameter('min', $search->q);
+            }
+
+            if(!empty($search->max)) {
+                $query = $query
+                    ->andWhere('p.price <= :max')
+                    ->setParameter('max', $search->q);
+            }
+
+            if(!empty($search->color)) {
+                $query = $query
+                    ->andWhere('c.id IN (:color)')
+                    ->setParameter('color', $search->color);
+            }
+
+            if(!empty($search->naming)) {
+                $query = $query
+                    ->andWhere('n.id IN (:naming)')
+                    ->setParameter('naming', $search->naming);
+            }
+
+            if(!empty($search->region)) {
+                $query = $query
+                    ->andWhere('r.id IN (:region)')
+                    ->setParameter('region', $search->region);
+            }
+
+
+        return $query->getQuery()->getResult();
     }
 
     // /**
