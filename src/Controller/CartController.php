801965d17be6ad386ad4f84338b7a75ed001f551
julien willette <julien.willette@gmail.com>
2021-02-05 09:53:11 +0100

    add stripe

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use Stripe\Stripe;
+use Slim\Http\Request;
+use Stripe\Checkout\Session;
 use App\Service\Cart\CartService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
@@ -38,4 +44,47 @@ class CartController extends AbstractController
         return $this->redirectToRoute('cart');
     }
 
+    /**
+     * @Route("/success", name="success")
+     */
+    public function success()
+    {
+        return $this->render('cart/success.html.twig');
+    }
+
+    /**
+     * @Route("/error", name="error")
+     */
+    public function error()
+    {
+        return $this->render('cart/error.html.twig');
+    }
+
+    /**
+     * @Route("create-checkout-session", name="checkout")
+     */
+    public function checkout()
+    {
+        \Stripe\Stripe::setApiKey('sk_test_51IHB1GA5Ie80voPhdRkjh2mF96KIQUKkE6f075k2SmdIXbgro6li1gY3iZFAOlljSua3RKyeR0D38EKIayijE7So00XhGsbYla');
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'eur',
+                'product_data' => [
+                  'name' => 'Wine',
+                ],
+                'unit_amount' => 50000,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => 'cart/success.html.twig',
+            'cancel_url' => 'cart/error.html.twig',
+        ]);
+        return new JsonResponse([ 'id' => $session->id ]);
+    }
+    
 }
+
